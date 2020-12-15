2e87b9ca577902b11f4be30a7060b65ca29d3473
saulo775 <59702227+saulo775@users.noreply.github.com>
2020-12-15 18:43:20 -0300

    criação e listagem de transações

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,32 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionRepository.create({
+      title,
+      type,
+      value,
+    });
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
